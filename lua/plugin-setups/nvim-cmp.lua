1fd292d2c0f6e36af5a0ee455929f025e06fdac3
Andrew Wright <agwright121@yahoo.com>
2023-10-01 19:28:56 -0400

    Correcting an issue with the setup of luasnip in lua/plugin-setups/nvim-cmp.lua.

--- lua/plugin-setups/nvim-cmp.lua
+++ lua/plugin-setups/nvim-cmp.lua
@@ -1,5 +1,8 @@
-local lspkind = require('lspkind')
-local cmp = require('cmp')
+local lspkind = require'lspkind'
+local cmp = require'cmp'
+local luasnip = require'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
 
 cmp.setup{
 	snippet = {
