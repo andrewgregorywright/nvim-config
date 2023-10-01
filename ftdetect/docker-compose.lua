fd6204089f786afcea9aef2949d34eb3f80fc1e8
Andrew Wright <agwright121@yahoo.com>
2023-10-01 18:58:57 -0400

    Updating the filetype for docker-compose.yaml files.

--- ftdetect/docker-compose.lua
+++ ftdetect/docker-compose.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_create_autocmd(
 	{ "BufEnter", "BufNewFile" },
-	{ pattern = "docker-compose*.yml", command = "setf dockercompose" }
+	{ pattern = "docker-compose*.yml", command = "setf yaml.docker-compose" }
 )
