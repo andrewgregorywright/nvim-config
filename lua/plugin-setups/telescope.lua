c4fbccee57923aabfa39cd36daf90681c93805a6
Andrew Wright <agwright121@yahoo.com>
2024-07-02 20:48:27 -0400

    Adding hidden files to telescope plugin.

--- lua/plugin-setups/telescope.lua
+++ lua/plugin-setups/telescope.lua
@@ -1,4 +1,14 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+
+telescope.setup {
+	pickers = {
+		find_files = {
+			hidden = true
+		}
+	}
+}
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
