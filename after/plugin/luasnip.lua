028e8a01366c665a77c7ca392e857a723ec2f3d1
Andrew Wright <agwright121@yahoo.com>
2024-09-21 05:20:05 -0400

    Adding stryc and console logging snippets.

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -7,20 +7,22 @@ local i = ls.insert_node
 local f = ls.function_node
 --]]
 
-ls.add_snippets('lua', {
-    --[[
-		s('func', {
-        t('function '),
-				i(1, 'functionName'),
-				t('('),
-				i(2, 'params'),
-				t('): '),
-				i(3, 'returnType'),
-				t({' {', '\t'}),
-        i(4, 'body'),
-				t({'', '}'})
-    }),
-		--]]
-		ls.parser.parse_snippet('func', 'ifunction ${1:name}(${2:args}) {\n\t${3}\n}')
+ls.add_snippets('typescript', {
+	--[[
+	s('func', {
+			t('function '),
+			i(1, 'functionName'),
+			t('('),
+			i(2, 'params'),
+			t('): '),
+			i(3, 'returnType'),
+			t({' {', '\t'}),
+			i(4, 'body'),
+			t({'', '}'})
+	}),
+	--]]
+	ls.parser.parse_snippet('stryc', 'try {\n\t$0\n} catch (err) {\n}'),
+	ls.parser.parse_snippet('scl', 'console.log(\'$0\')'),
+	ls.parser.parse_snippet('sce', 'console.error(\'$0\')'),
 })
 
