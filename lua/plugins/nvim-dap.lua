8a6ff65d56951c5844aa9b23844e839c3a25de93
Andrew Wright <agwright121@yahoo.com>
2025-07-02 06:51:07 -0400

    Minor modifications.

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -9,6 +9,7 @@ return {
 		},
 		'mfussenegger/nvim-dap-python',
 		'theHamsta/nvim-dap-virtual-text',
+		'microsoft/vscode-js-debug',
 	},
 	config = function()
 		local dap = require 'dap'
@@ -34,6 +35,24 @@ return {
 		dapui.setup()
 
 		require('dap-python').setup('~/.local/share/virtualenvs/python-3.10-debugpy/bin/python')
+		require("dap-vscode-js").setup({
+			-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+			-- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+			-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+			adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+			-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+			-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+			-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+		})
+
+		table.insert(require('dap').configurations.typescript, {
+			type = 'pwa-node',
+			request = 'launch',
+			name = 'Launch file (TS)',
+			program = '${file}',
+			cwd = '${workspaceFolder}',
+			-- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+		})
 		--[[
 		table.insert(require('dap').configurations.python, {
 			type = 'python',
